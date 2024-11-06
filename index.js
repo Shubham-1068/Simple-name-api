04765288eb1908c2f47b505f84fe71914468d5cd
SHUBHAM <shubhamraj1068@gmail.com>
2024-11-06 12:44:47 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const data = require("./data.json")
+
+const port = 8000;
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    const result = `<b>${data.map((e)=>{
+        return `<div>Name: ${e.name}, Age: ${e.age}</div>`
+    }).join("")}</b>`
+
+    res.send(result)
+})
+
+app.get("/user", (req,res)=>{
+    res.json(data)
+})
+
+app.listen(port,()=>{console.log(`Server started at port: ${port}`)})
